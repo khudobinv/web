9bb6a0b5760826b4170f048b71edac21560e9e2d
khudobin-v <162823979+khudobin-v@users.noreply.github.com>
2024-07-01 19:23:10 +0300

    Изменение стилей

--- web5/index.php
+++ web5/index.php
@@ -15,14 +15,14 @@
   }
 
   if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $fio = (!empty($_COOKIE['fio_error']) ? $_COOKIE['fio_error'] : '');
+    $fullName = (!empty($_COOKIE['fullName_error']) ? $_COOKIE['fullName_error'] : '');
     $phone = (!empty($_COOKIE['phone_error']) ? $_COOKIE['phone_error'] : '');
     $email = (!empty($_COOKIE['email_error']) ? $_COOKIE['email_error'] : '');
     $birthday = (!empty($_COOKIE['birthday_error']) ? $_COOKIE['birthday_error'] : '');
     $gender = (!empty($_COOKIE['gender_error']) ? $_COOKIE['gender_error'] : '');
     $like_lang = (!empty($_COOKIE['like_lang_error']) ? $_COOKIE['like_lang_error'] : '');
     $biography = (!empty($_COOKIE['biography_error']) ? $_COOKIE['biography_error'] : '');
-    $oznakomlen = (!empty($_COOKIE['oznakomlen_error']) ? $_COOKIE['oznakomlen_error'] : '');
+    $agreement = (!empty($_COOKIE['agreement_error']) ? $_COOKIE['agreement_error'] : '');
 
     $errors = array();
     $messages = array();
@@ -59,14 +59,14 @@
       }
     }
     
-    val_empty('fio', $fio);
+    val_empty('fullName', $fullName);
     val_empty('phone', $phone);
     val_empty('email', $email);
     val_empty('birthday', $birthday);
     val_empty('gender', $gender);
     val_empty('like_lang', $like_lang);
     val_empty('biography', $biography);
-    val_empty('oznakomlen', $oznakomlen);
+    val_empty('agreement', $agreement);
     
     $like_langsa = explode(',', $values['like_lang']);
 
@@ -89,14 +89,14 @@
         foreach($dbL->fetchAll(PDO::FETCH_ASSOC) as $item){
           $like_langsa[] = $item['name'];
         }
-        setVal('fio', $fet['fio']);
+        setVal('fullName', $fet['fullName']);
         setVal('phone', $fet['phone']);
         setVal('email', $fet['email']);
         setVal('birthday', date("Y-m-d", $fet['birthday']));
         setVal('gender', $fet['gender']);
         setVal('like_lang', $like_lang);
         setVal('biography', $fet['biography']);
-        setVal('oznakomlen', $fet['oznakomlen']);
+        setVal('agreement', $fet['agreement']);
       }
       catch(PDOException $e){
         print('Error : ' . $e->getMessage());
@@ -111,24 +111,24 @@
   }
   // Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
   else {
-    $fio = (!empty($_POST['fio']) ? $_POST['fio'] : '');
+    $fullName = (!empty($_POST['fullName']) ? $_POST['fullName'] : '');
     $phone = (!empty($_POST['phone']) ? $_POST['phone'] : '');
     $email = (!empty($_POST['email']) ? $_POST['email'] : '');
     $birthday = (!empty($_POST['birthday']) ? $_POST['birthday'] : '');
     $gender = (!empty($_POST['gender']) ? $_POST['gender'] : '');
     $like_lang = (!empty($_POST['like_lang']) ? $_POST['like_lang'] : '');
     $biography = (!empty($_POST['biography']) ? $_POST['biography'] : '');
-    $oznakomlen = (!empty($_POST['oznakomlen']) ? $_POST['oznakomlen'] : '');
+    $agreement = (!empty($_POST['agreement']) ? $_POST['agreement'] : '');
 
     if(isset($_POST['logout_form'])){
-      del_cook('fio', 1);
+      del_cook('fullName', 1);
       del_cook('phone', 1);
       del_cook('email', 1);
       del_cook('birthday', 1);
       del_cook('gender', 1);
       del_cook('like_lang', 1);
       del_cook('biography', 1);
-      del_cook('oznakomlen', 1);
+      del_cook('agreement', 1);
       session_destroy();
       header('Location: ./');
       exit();
@@ -155,9 +155,9 @@
       return $res;
     }
     
-    if(!val_empty('fio', 'Заполните поле', empty($fio))){
-      if(!val_empty('fio', 'Длина поля > 255 символов', strlen($fio) > 255)){
-        val_empty('fio', 'Поле не соответствует требованиям: <i>Фамилия Имя (Отчество)</i>, кириллицей', !preg_match('/^([а-яёА-ЯЁ]+-?[а-яёА-ЯЁ]+)( [а-яёА-ЯЁ]+-?[а-яёА-ЯЁ]+){1,2}$/Diu', $fio));
+    if(!val_empty('fullName', 'Заполните поле', empty($fullName))){
+      if(!val_empty('fullName', 'Длина поля > 255 символов', strlen($fullName) > 255)){
+        val_empty('fullName', 'Поле не соответствует требованиям: <i>Фамилия Имя (Отчество)</i>, кириллицей', !preg_match('/^([а-яёА-ЯЁ]+-?[а-яёА-ЯЁ]+)( [а-яёА-ЯЁ]+-?[а-яёА-ЯЁ]+){1,2}$/Diu', $fullName));
       }
     }
     if(!val_empty('phone', 'Заполните поле', empty($phone))){
@@ -195,7 +195,7 @@
     if(!val_empty('biography', 'Заполните поле', empty($biography))){
       val_empty('biography', 'Длина текста > 65 535 символов', strlen($biography) > 65535);
     }
-    val_empty('oznakomlen', "Ознакомьтесь с контрактом", empty($oznakomlen));
+    val_empty('agreement', "Ознакомьтесь с контрактом", empty($agreement));
     
     if ($error) {
       // При наличии ошибок перезагружаем страницу и завершаем работу скрипта.
@@ -204,21 +204,21 @@
     }
     else {
       // Удаляем Cookies с признаками ошибок.
-      del_cook('fio');
+      del_cook('fullName');
       del_cook('phone');
       del_cook('email');
       del_cook('birthday');
       del_cook('gender');
       del_cook('like_lang');
       del_cook('biography');
-      del_cook('oznakomlen');
+      del_cook('agreement');
     }
   
     // Проверяем меняются ли ранее сохраненные данные или отправляются новые.
     if ($log) {
       
-      $stmt = $db->prepare("UPDATE form_data SET fio = ?, phone = ?, email = ?, birthday = ?, gender = ?, biography = ? WHERE user_id = ?");
-      $stmt->execute([$fio, $phone, $email, strtotime($birthday), $gender, $biography, $_SESSION['user_id']]);
+      $stmt = $db->prepare("UPDATE form_data SET fullName = ?, phone = ?, email = ?, birthday = ?, gender = ?, biography = ? WHERE user_id = ?");
+      $stmt->execute([$fullName, $phone, $email, strtotime($birthday), $gender, $biography, $_SESSION['user_id']]);
 
       $stmt = $db->prepare("DELETE FROM form_data_lang WHERE id_form = ?");
       $stmt->execute([$_SESSION['form_id']]);
@@ -247,8 +247,8 @@
         $stmt->execute([$login, $mpassword]);
         $user_id = $db->lastInsertId();
 
-        $stmt = $db->prepare("INSERT INTO form_data (user_id, fio, phone, email, birthday, gender, biography) VALUES (?, ?, ?, ?, ?, ?, ?)");
-        $stmt->execute([$user_id, $fio, $phone, $email, strtotime($birthday), $gender, $biography]);
+        $stmt = $db->prepare("INSERT INTO form_data (user_id, fullName, phone, email, birthday, gender, biography) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt->execute([$user_id, $fullName, $phone, $email, strtotime($birthday), $gender, $biography]);
         $fid = $db->lastInsertId();
 
         $stmt1 = $db->prepare("INSERT INTO form_data_lang (id_form, id_lang) VALUES (?, ?)");
@@ -260,14 +260,14 @@
         print('Error : ' . $e->getMessage());
         exit();
       }
-      setcookie('fio_value', $fio, time() + 24 * 60 * 60 * 365);
+      setcookie('fullName_value', $fullName, time() + 24 * 60 * 60 * 365);
       setcookie('phone_value', $phone, time() + 24 * 60 * 60 * 365);
       setcookie('email_value', $email, time() + 24 * 60 * 60 * 365);
       setcookie('birthday_value', $birthday, time() + 24 * 60 * 60 * 365);
       setcookie('gender_value', $gender, time() + 24 * 60 * 60 * 365);
       setcookie('like_value', $like, time() + 24 * 60 * 60 * 365);
       setcookie('biography_value', $biography, time() + 24 * 60 * 60 * 365);
-      setcookie('oznakomlen_value', $oznakomlen, time() + 24 * 60 * 60 * 365);
+      setcookie('agreement_value', $agreement, time() + 24 * 60 * 60 * 365);
     }
 
     // Сохраняем куку с признаком успешного сохранения.
--- web5/login.php
+++ web5/login.php
@@ -66,23 +66,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="libs/bootstrap-4.0.0-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style8.css">
-    <script src="libs/jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Задание 5</title>
 </head>
-<body>
-  <div class="pform pformAuth">
-    <form action="" method="post">
+<body class="bg-gray-900 h-screen flex items-center justify-center">
+  <div class="flex flex-col gap-2 border-2 border-white/10 rounded-xl text-white p-4 w-1/4">
+    <form class="flex flex-col gap-2" method="post">
       <div class="message" style="color: red;"><?php echo $error; ?></div>
-      <h3>Авторизация</h3>
+        <h3 class="text-3xl font-semibold text-center">Авторизация</h3>
         <div>
-          <input class="w100" type="text" name="login" placeholder="Логин">
+          <input class="rounded-lg p-1 border-2 border-white/10 bg-gray-900 text-sm w-full" type="text" name="login" placeholder="Логин">
         </div>
         <div>
-          <input class="w100" type="text" name="password" placeholder="Пароль">
+          <input class="rounded-lg p-1 border-2 border-white/10 bg-gray-900 text-sm w-full" type="text" name="password" placeholder="Пароль">
         </div>
-        <button type="submit">Войти</button>
+        <button type="submit" class="p-2 rounded-lg bg-green-600 text-sm hover:bg-opacity-90 transition-colors hover:shadow-lg">Войти</button>
     </form>
   </div>
 </body>
