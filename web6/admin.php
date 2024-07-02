87b4244b2315500cfd15a8887106b47a9432e48f
khudobin-v <162823979+khudobin-v@users.noreply.github.com>
2024-07-02 13:16:44 +0300

    Правка ошибок

--- web6/admin.php
+++ web6/admin.php
@@ -32,6 +32,7 @@ if (!$haveAdmin) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Задание 6 (админка)</title>
 </head>
